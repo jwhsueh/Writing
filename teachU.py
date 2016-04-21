f20cd3f2f8a9eb0e5ab658ffd5123a51f142a280
jwhsueh <j35234@gmail.com>
2016-04-21 14:16:01 -0700

    Create teachU.py

---
+++ teachU.py
@@ -0,0 +1,18 @@
+# class can easily pass a group vairables into a function
+
+class Sballball:
+  blue = 1
+  green = 2
+  red = 3
+  
+print Sballball.blue
+# output should be 1
+
+## Here I define a function
+def SumUpBallballz(InputAClass):
+  Sballball_01 = InputAClass.blue
+  Sballball_02 = InputAClass.green
+  Sballball_03 = InputAClass.red
+  
+  SumUp = Sballball_01+Sballball_02+Sballball_03
+  
